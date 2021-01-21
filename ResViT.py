e6444d3be907c80d7fe3ee9db9eb0e9fa9a6ebf7
Thijs Werrij <thijswerrij@gmail.com>
2021-01-21 17:56:59 +0100

    Added main function, ResViT can now be used as module

--- ResViT.py
+++ ResViT.py
@@ -234,38 +234,12 @@ class ViTResNet(nn.Module):
         
         return x
 
-
-BATCH_SIZE_TRAIN = 100
-BATCH_SIZE_TEST = 100
-
-DL_PATH = "C:\Pytorch\Spyder\CIFAR10_data" # Use your own path
-# CIFAR10: 60000 32x32 color images in 10 classes, with 6000 images per class
-transform = torchvision.transforms.Compose(
-     [torchvision.transforms.RandomHorizontalFlip(),
-     torchvision.transforms.RandomRotation(10, resample=PIL.Image.BILINEAR),
-     torchvision.transforms.RandomAffine(8, translate=(.15,.15)),
-     torchvision.transforms.ToTensor(),
-     torchvision.transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
-
-
-train_dataset = torchvision.datasets.CIFAR10(DL_PATH, train=True,
-                                        download=True, transform=transform)
-
-test_dataset = torchvision.datasets.CIFAR10(DL_PATH, train=False,
-                                       download=True, transform=transform)
-
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE_TRAIN,
-                                          shuffle=True)
-
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE_TEST,
-                                         shuffle=False)
-
 def train(model, optimizer, data_loader, loss_history):
     total_samples = len(data_loader.dataset)
     model.train()
 
     for i, (data, target) in enumerate(data_loader):
-        data, target = data.to(device), target.to(device)
+        data, target = data.to(device), target.to(device, dtype=torch.int64)
         optimizer.zero_grad()
         output = F.log_softmax(model(data), dim=1)
         loss = F.nll_loss(output, target)
@@ -287,7 +261,7 @@ def evaluate(model, data_loader, loss_history):
 
     with torch.no_grad():
         for data, target in data_loader:
-            data, target = data.to(device), target.to(device)
+            data, target = data.to(device), target.to(device, dtype=torch.int64)
             output = F.log_softmax(model(data), dim=1)
             loss = F.nll_loss(output, target, reduction='sum')
             _, pred = torch.max(output, dim=1)
@@ -302,27 +276,53 @@ def evaluate(model, data_loader, loss_history):
           '{:5}'.format(total_samples) + ' (' +
           '{:4.2f}'.format(100.0 * correct_samples / total_samples) + '%)\n')
 
+DL_PATH = "C:\Pytorch\Spyder\CIFAR10_data" # Use your own path
+# CIFAR10: 60000 32x32 color images in 10 classes, with 6000 images per class
+transform = torchvision.transforms.Compose(
+     [torchvision.transforms.RandomHorizontalFlip(),
+     torchvision.transforms.RandomRotation(10, resample=PIL.Image.BILINEAR),
+     torchvision.transforms.RandomAffine(8, translate=(.15,.15)),
+     torchvision.transforms.ToTensor(),
+     torchvision.transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+
+BATCH_SIZE_TRAIN = 100
+BATCH_SIZE_TEST = 100
 N_EPOCHS = 150
 
+if __name__ == "__main__":
+    
+    train_dataset = torchvision.datasets.CIFAR10(DL_PATH, train=True,
+                                        download=True, transform=transform)
 
-model = ViTResNet(BasicBlock, [3, 3, 3]).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.003)
+    test_dataset = torchvision.datasets.CIFAR10(DL_PATH, train=False,
+                                       download=True, transform=transform)
 
-#optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate,momentum=.9,weight_decay=1e-4)
-#lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=[35,48],gamma = 0.1)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE_TRAIN,
+                                          shuffle=True)
 
-train_loss_history, test_loss_history = [], []
-for epoch in range(1, N_EPOCHS + 1):
-    print('Epoch:', epoch)
-    start_time = time.time()
-    train(model, optimizer, train_loader, train_loss_history)
-    print('Execution time:', '{:5.2f}'.format(time.time() - start_time), 'seconds')
-    evaluate(model, test_loader, test_loss_history)
-
-print('Execution time')
-
-PATH = ".\ViTRes.pt" # Use your own path
-torch.save(model.state_dict(), PATH)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE_TEST,
+                                         shuffle=False)
+    
+    model = ViTResNet(BasicBlock, [3, 3, 3]).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.003)
+    
+    #optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate,momentum=.9,weight_decay=1e-4)
+    #lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=[35,48],gamma = 0.1)
+    
+    init_time = time.time()
+    train_loss_history, test_loss_history = [], []
+    for epoch in range(1, N_EPOCHS + 1):
+        print('Epoch:', epoch)
+        start_time = time.time()
+        train(model, optimizer, train_loader, train_loss_history)
+        print('Execution time:', '{:5.2f}'.format(time.time() - start_time), 'seconds')
+        evaluate(model, test_loader, test_loss_history)
+    
+    minutes, seconds = divmod(time.time() - init_time, 60)
+    print('Total execution time:', '{:.0f}m {:.2f}s'.format(minutes, seconds))
+    
+    PATH = ".\ViTRes.pt" # Use your own path
+    torch.save(model.state_dict(), PATH)
 
 
 # =============================================================================
